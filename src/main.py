66dcf6356200692fd90cd3c51111bf17e764c106
s-jaiswal <s.jaiswal@salesforce.com>
2023-08-22 09:20:21 +0530

    Troubleshoot suggestion script

--- src/main.py
+++ src/main.py
@@ -6,33 +6,22 @@ import os
 
 os.environ["OPENAI_API_KEY"] = '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'
 
-def construct_index(directory_path):
-    max_input_size = 4096
-    num_outputs = 512
-    max_chunk_overlap = 20
-    chunk_size_limit = 600
-
-    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="text-davinci-003", max_tokens=num_outputs))
-
-    documents = SimpleDirectoryReader(directory_path).load_data()
-
-    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
-
-    index.save_to_disk('index.json')
-
-    return index
+# os.system("corecli core:build");
 
 def chatbot(input_text):
     index = GPTSimpleVectorIndex.load_from_disk('index.json')
     response = index.query(input_text, response_mode="compact")
     return response.response
 
-iface = gr.Interface(fn=chatbot,
-                     inputs=gr.inputs.Textbox(lines=7, label="Enter your text"),
-                     outputs="text",
-                     title="My AI Chatbot")
+# read error from output.log file
+file1 = open(r"/Users/s.jaiswal/Desktop/demo/error.log", "r+")
+r = file1.readline()
 
-index = construct_index("/Users/s.jaiswal/Desktop/demo/train_data")
-iface.launch(share=True)
+#query openapi and get the suggestion for error
+response = chatbot(r);
+
+print("*** corecli build failed with some issues ***");
+print("*** please try this suggestion to resolve build issue ***")
+
+#print the response reveived from openapi
+print(response);

783d7f5657351d6acb1fdd71c1f7fc26d1995b42
sumitjaiswal35 <43412751+sumitjaiswal35@users.noreply.github.com>
2023-08-23 15:12:29 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ import gradio as gr
 import sys
 import os
 
-os.environ["OPENAI_API_KEY"] = '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'
+os.environ["OPENAI_API_KEY"] = '******'
 
 # os.system("corecli core:build");
 
